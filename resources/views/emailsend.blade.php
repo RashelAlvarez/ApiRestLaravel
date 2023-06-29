1114276a08c480a83fd40b9e44ffa3226dfa5850
RashelAlvarez <rashelalvarez21@gmail.com>
2023-06-28 23:25:10 -0400

    cambios en el formato de correo

--- resources/views/emailsend.blade.php
+++ resources/views/emailsend.blade.php
@@ -1,1 +1,9 @@
 <p>Sus datos han sido enviados, pronto sera contactado por el administrador del sitio web</p>
+
+
+<p><b>Contact name:</b>  {{$msg['name']}} </p>
+<p><b> Email: </b> {{$msg['email']}}</p>
+<p><b> Message: </b> {{$msg['message']}}</p>
+<p><b> Country: </b> {{$msg['country']}}</p>
+<p><b> State: </b> {{$msg['state']}}</p>
+<p><b> City: </b> {{$msg['city']}}</p>
